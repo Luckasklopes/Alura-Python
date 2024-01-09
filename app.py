2edd3f7a4213c6f0db64fde62236fac639a1f1ff
Lucas Lopes <125325077+Luckasklopes@users.noreply.github.com>
2024-01-09 14:45:47 -0500

    adição de match no codigo principal, mais desafios concluidos

--- app.py
+++ app.py
@@ -25,16 +25,28 @@ def escolher_opcao():
     #opcao_escolhida = int(opcao_escolhida)
 
     #print("Você escolheu a opção", opcao_escolhida)
-    print(f"Você escolheu a opção {opcao_escolhida}")
+    #print(f"Você escolheu a opção {opcao_escolhida}")
 
-    if opcao_escolhida == 1:
-        print("cadastrar restaurante")
-    elif opcao_escolhida == 2:
-        print("Listar restaurantes")
-    elif opcao_escolhida == 3:
-        print("Ativar restaurante")
-    else:
-        finalizar_app()
+    match opcao_escolhida:
+        case 1:
+            print("cadastrar restaurante")
+        case 2:
+            print("listar restaurantes")
+        case 3:
+            print("ativar restaurante")
+        case 0:
+            finalizar_app()
+        case _:
+            print("opção invalida")
+
+#    if opcao_escolhida == 1:
+#        print("cadastrar restaurante")
+#    elif opcao_escolhida == 2:
+#        print("Listar restaurantes")
+#    elif opcao_escolhida == 3:
+#        print("Ativar restaurante")
+#    else:
+#        finalizar_app()
 
 def main():
     exibir_logo()
--- desafios.py
+++ desafios.py
@@ -1,3 +1,5 @@
+import os
+
 # 1
 print("Python na escola de programação da Alura.")
 
@@ -18,3 +20,57 @@ print("""
 # 4
 pi = 3.141592
 print(f"O valor arredondado de pi é {pi:.2f}")
+
+input("")
+os.system("cls")
+
+#5
+valor = int(input("digite um numero..: "))
+
+if valor == 0:
+    print(f"0 é par ou impar?")
+elif valor % 2 == 0:
+    print(f"o numero {valor} é par")
+else:
+    print(f"o numero {valor} é impar")
+
+#6
+idade = int(input("digite sua idade..: "))
+
+if idade < 0:
+    print(f"{valor} não é uma idade valida")
+elif idade <= 12:
+    print(f"com {idade} anos você é uma criança")
+elif idade <= 18:
+    print(f"com {idade} anos você é um adolescente")
+else:
+    print(f"com {idade} anos você ja é um adulto")
+
+#7
+email = "zezinho@gmail.com"
+senha = "teste123123"
+
+email_digitado = input("digite seu email..: ")
+senha_digitada = input("digite sua senha..: ")
+
+if email_digitado == email:
+    if senha_digitada == senha:
+        print("Logado com sucesso")
+    else:
+        print("senha incorreta")
+else:
+    print("email incorreto")
+
+#8
+x = int(input("digite as cordenadas x..: "))
+y = int(input("digite as cordenadas y..: "))
+if x > 0 and y > 0:
+    print("você está no primeiro quadrante")
+elif x < 0 and y > 0:
+    print("você está no segundo quadrante")
+elif x < 0 and y < 0:
+    print("você está no terceiro quadrante")
+elif x > 0 and y < 0:
+    print("você está no quarto quadrante")
+else:
+    print("você está na origem")
