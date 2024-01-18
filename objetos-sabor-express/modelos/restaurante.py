5908da587837c5c396258cc5ffc2831b41f7d25f
Lucas Lopes <125325077+Luckasklopes@users.noreply.github.com>
2024-01-18 17:13:51 -0500

    re-estruturação do codigo

--- objetos-sabor-express/modelos/restaurante.py
+++ objetos-sabor-express/modelos/restaurante.py
@@ -1,11 +1,21 @@
 class Restaurante:
-    nome = ""
-    categoria = ""
-    ativo = False
+    restaurantes = [] #lista de restaurantes
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self) #todo objeto criado entra na lista de restaurantes
+    
+    def __str__(self): # define um retorno especial ao chamar o nome do restaurantes
+        return(f"{self.nome} | {self.categoria}")
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}")
 
-restaurante_praca = Restaurante()
-restaurante_pizza = Restaurante()
+restaurante_praca = Restaurante("Praça", "Gourmet")
+restaurante_pizza = Restaurante("Pizza express", "Italiano")
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(dir(restaurante_praca))
+Restaurante.listar_restaurantes()
