f63ea8f64500980bbe92b9ef15e502a3647bfd91
Lucas Lopes <125325077+Luckasklopes@users.noreply.github.com>
2024-02-01 13:24:43 -0500

    finalização do curso 2

--- objetos-sabor-express/app.py
+++ objetos-sabor-express/app.py
@@ -1,9 +1,12 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante("Praça", "gourmet")
-restaurante_praca.receber_avaliacao("Lucas", 9)
+restaurante_praca.receber_avaliacao("Lucas", 6)
 restaurante_praca.receber_avaliacao("Pedro", 6)
-restaurante_praca.receber_avaliacao("Emy", 10)
+restaurante_praca.receber_avaliacao("Emy", 6)
+restaurante_praca.receber_avaliacao("Joao", 11)
+
+restaurante_sushi = Restaurante("sushi", "japonesa")
 
 def main():
     Restaurante.listar_restaurantes()
